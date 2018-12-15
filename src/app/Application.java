e5ae263e13ab656c93614a623f7b0ebf5dedc50c
Dawid97 <45856213+DawidSobierajski@users.noreply.github.com>
2018-12-15 14:17:14 +0100

    Update Application.java

--- src/app/Application.java
+++ src/app/Application.java
@@ -26,29 +26,29 @@ public class Application {
         switch (wybor)
         {
             case "P":
-            try
-            {
-                System.out.println("Wybrałeś plik.");
-
-                ArrayList<String> instructionLines = new ArrayList<>(Files.readAllLines(new File(args[0]).toPath(), Charset.forName("UTF-8")));
-                instructionLines.set(0, instructionLines.get(0).substring(1));
-
-                for (String instructionLine : instructionLines)
+                try
                 {
-                    try
+                    System.out.println("Wybrałeś plik.");
+   
+                 ArrayList<String> instructionLines = new ArrayList<>(Files.readAllLines(new File(args[0]).toPath(), Charset.forName("UTF-8")));
+                    instructionLines.set(0, instructionLines.get(0).substring(1));
+
+                    for (String instructionLine : instructionLines)
                     {
-                        i.interpret(instructionLine);
-                    }
-                    catch (Exception e)
-                    {
-                        System.out.println(e.getMessage());
+                        try
+                        {
+                            i.interpret(instructionLine);
+                        }
+                        catch (Exception e)
+                        {
+                            System.out.println(e.getMessage());
+                        }
                     }
                 }
-            }
-            catch (IOException e)
-            {
-                e.printStackTrace();
-            }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
             break;
 
             case "I":
